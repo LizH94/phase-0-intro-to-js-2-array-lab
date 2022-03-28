526f0ab36e1636263d01bf91c383a2b6c51effc8
Elizabeth Hammond <lizh94@aol.com>
2022-03-28 01:41:31 -0400

    commit tree fde3c4ee0dd20725e867ad4b7e7b833e0d3ceee2

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
+}
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
+}
+
+function appendCat(name){
+    const copyCats = [...cats, name];
+    return copyCats;
+}
+
+function prependCat(name){
+    const copyCats2 = [name, ...cats];
+    return copyCats2;
+}
+
+function removeLastCat(){
+    const copyCats3 = cats.slice(0, cats.length - 1);
+    return copyCats3;
+}
+
+function removeFirstCat(){
+    const copyCats4 = cats.slice(1);
+    return copyCats4;
+}
